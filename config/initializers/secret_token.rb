04ec357de5f1596295a663a4e8fc088c1ba14e52
Andrew Swenson <SwenA56@gmail.com>
2013-06-10 04:58:34 -0500

    StartOfProject

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoalOfProjectIsToSendToGitHub::Application.config.secret_token = '454371d16352aeab475bcc799acf15ad77b69c6148928562eac12979ede63786c0e71860978154be3609b60814aa5254fd50d493595ee099ec2137c2a4ec79cd'
